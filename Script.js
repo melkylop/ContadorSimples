b54771209e2dcf8299d642b726c0d2b087e25060
Melky Lopes <64280680+melkylop@users.noreply.github.com>
2022-05-02 17:48:12 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,12 @@
+var currentNumberWapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWapper.innerHTML = currentNumber;
+}
